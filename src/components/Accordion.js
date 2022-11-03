4c6664293259febfedb76b1ec96cac4600cb19db
Aditria Pardana <aditria.pardana20@gmail.com>
2022-11-03 15:03:54 +0700

    update semantic ui

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Accordion({ items }) {
-  return <div>{items.length}</div>;
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={index}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 }
 
 export default Accordion;
